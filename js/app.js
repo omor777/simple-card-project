e62ab9a2824fe4af3988a5edb248e93bca9a5569
omor777 <omor0843@gmail.com>
2024-02-16 01:23:56 +0600

    project completed

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ function main() {
   const addToCardButtons = document.getElementsByClassName("add-btn");
   for (const addToCardBtn of addToCardButtons) {
     addToCardBtn.addEventListener("click", function (e) {
+      // disabled card button
+      e.target.disabled = true;
+      e.target.classList.add('bg-gray-400')
+      e.target.classList.add('text-black')
+      e.target.parentNode.parentNode.classList.add("bg-gray-500");
+
       //  update cart count
       const CartCount = getInnerText("cart-count");
       const totalCartCount = convertStringToNumber(CartCount) + 1;
@@ -18,6 +24,16 @@ function main() {
       const title = e.target.parentNode.firstElementChild.innerText;
       const price = e.target.parentNode.children[1].firstElementChild.innerText;
 
+      //   update total budget
+      const budget = convertStringToNumber(getInnerText("budget"));
+      const totalBudget = budget - convertStringToNumber(price);
+      if (totalBudget < 0) {
+        alert("budget limit cross");
+        return;
+      }
+      setInnerText("budget", totalBudget);
+      //   console.log(totalBudget);
+
       const li = document.createElement("li");
       const p = document.createElement("p");
       p.innerText = title;
@@ -27,8 +43,26 @@ function main() {
       li.appendChild(p);
       li.appendChild(p2);
       selectedPlaceContainer.appendChild(li);
-
-     
+      // update total cost
+      const totalCost =
+        convertStringToNumber(getInnerText("total-cost")) +
+        convertStringToNumber(price);
+      setInnerText("total-cost", totalCost);
+      //  update grand total
+      calculateGrandTotal();
     });
+  }
+}
+
+function calculateGrandTotal(category) {
+  let total = convertStringToNumber(getInnerText("total-cost"));
+  if (category === "bus") {
+    setInnerText("grand-total", total + 100);
+  } else if (category === "train") {
+    setInnerText("grand-total", total - 200);
+  } else if (category === "flight") {
+    setInnerText("grand-total", total + 500);
+  } else {
+    setInnerText("grand-total", total);
   }
 }
